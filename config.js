1fbdb81ad83dd3e1463cf5c0c12e3c3053e27d1c
mdlufy <german.panov.2018@mail.ru>
2022-08-25 18:51:22 +0300

    chore: separate mongo conn options to config

--- config.js
+++ config.js
@@ -1,3 +1,4 @@
 module.exports = {
     secret: "SECRET_KEY_RANDOM",
+    mongoConfig: `mongodb+srv://mdlufy:magnesia2853328533@cluster1.xmsx5kj.mongodb.net/auth_roles?retryWrites=true&w=majority`,
 }
--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const authRouter = require('./authRouter')
 const PORT = process.env.PORT || 5000;
+const {mongoConfig} = require('./config'); 
 
 const app = express();
 
@@ -10,7 +11,7 @@ app.use('/auth', authRouter);
 
 const start = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://mdlufy:magnesia2853328533@cluster1.xmsx5kj.mongodb.net/auth_roles?retryWrites=true&w=majority`);
+        await mongoose.connect(mongoConfig);
         app.listen(PORT, () => console.log(`server start on port ${PORT}`));
     } catch (e) {
         console.log(e);
